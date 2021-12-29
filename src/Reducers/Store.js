74124eb1d6ce30bf3f675bae87ddfc7a7d2b8735
Kurosh <KuroBeowulF@gmail.com>
2021-12-29 21:51:55 +0330

    redux-persist

--- src/Reducers/Store.js
+++ src/Reducers/Store.js
@@ -1,6 +1,13 @@
 import { createStore } from "redux";
 import { rootReducer } from "./rootReducers";
+import { persisStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
-const store = createStore(rootReducer);
-
-export default store;
+const persistCofig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistCofig, rootReducer);
+export const store = createStore(persistedReducer);
+export const persistore = persistStore(store);
